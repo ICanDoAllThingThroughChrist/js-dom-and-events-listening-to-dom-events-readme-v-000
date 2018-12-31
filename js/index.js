7473936d18d114cbefe6c1f64d684019e087044b
charlie  lee <ctlee9096@gmail.com>
2018-12-31 22:41:08 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -17,3 +17,29 @@ input.addEventListener('keydown', function(e) {
 });
 
 let divs = document.querySelectorAll('div');
+function bubble(e) {
+  console.log(this.firstChild.nodeValue.trim() + ' bubbled');
+}
+
+for (let i = 0; i < divs.length; i++) {
+  divs[i].addEventListener('click', bubble);
+}
+function capture(e) {
+  console.log(this.firstChild.nodeValue.trim() + ' captured');
+}
+
+for (let i = 0; i < divs.length; i++) {
+  // set the third argument to `true`!
+  divs[i].addEventListener('click', capture, true);
+}
+function bubble(e) {
+  // stop! that! propagation!
+  e.stopPropagation();
+
+  console.log(this.firstChild.nodeValue.trim() + ' bubbled');
+}
+
+for (let i = 0; i < divs.length; i++) {
+  divs[i].addEventListener('click', bubble);
+}
+
